3e51947bf11a3e769c3812325b250d00c6b5039f
Piotr Mankowski <pmanko@uw.edu>
2025-06-12 18:13:41 +0000

    Updates to local setup

--- mk.sh
+++ mk.sh
@@ -6,28 +6,28 @@
 #./instant project init --env-file .env
 
 #reverse proxy
-# ./instant package remove -n reverse-proxy-nginx --env-file .env
+#./instant package remove -n reverse-proxy-nginx --env-file .env
 # ./instant package init -n reverse-proxy-nginx --env-file .env
 # ./instant package down -n reverse-proxy-nginx --env-file .env
 # ./instant package up -n reverse-proxy-nginx --env-file .env
 
 #openhim
 
-# ./instant package remove -n interoperability-layer-openhim --env-file .env
-# ./instant package init -n interoperability-layer-openhim --env-file .env -d
+./instant package remove -n interoperability-layer-openhim --env-file .env
+./instant package init -n interoperability-layer-openhim --env-file .env -d
 # ./instant package down -n interoperability-layer-openhim --env-file .env
 # ./instant package up -n interoperability-layer-openhim --env-file .env -d
 
 #mysql 
 
-./instant package remove -n database-mysql --env-file .env
-./instant package init -n database-mysql --env-file .env -d
+# ./instant package remove -n database-mysql --env-file .env
+# ./instant package init -n database-mysql --env-file .env -d
 # ./instant package down -n database-mysql --env-file .env
 # ./instant package up -n database-mysql --env-file .env
 
 # #isanteplus
-./instant package remove -n emr-isanteplus --env-file .env
-./instant package init -n emr-isanteplus --env-file .env -d
+# ./instant package remove -n emr-isanteplus --env-file .env
+# ./instant package init -n emr-isanteplus --env-file .env -d
 #./instant package down -n emr-isanteplus --env-file .env
 #./instant package up -n emr-isanteplus --env-file .env
 
